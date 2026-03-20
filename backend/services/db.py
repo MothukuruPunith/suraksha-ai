edd7808c5ac2f88cc68db2aa23a73b33c0103016
Mothukuru Punith <punithmothukuru970@gmail.com>
2026-03-20 16:57:56 +0530

    Remove hardcoded MySQL password in db.py

--- backend/services/db.py
+++ backend/services/db.py
@@ -4,6 +4,6 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Punith@2005",  # put your mysql password if any
+        password="",  # put your mysql password if any
         database="suraksha_ai"
     )
